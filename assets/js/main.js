5811e6d24105fd6d6194f31ce60f8edf1ca3c4ca
Phillaf <phil.laf@gmail.com>
2015-07-03 05:00:09 -0400

    missing coma in bower file

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,9 @@ requirejs.config({
         "jsx-requirejs-plugin": "../vendor/jsx-requirejs-plugin/js/jsx",
         react: "../vendor/react/react",
         "requirejs-text": "../vendor/requirejs-text/text",
-        less: "../vendor/less/dist/less"
+        less: "../vendor/less/dist/less",
+        bootstrap: "../vendor/bootstrap/dist/js/bootstrap",
+        jquery: "../vendor/jquery/dist/jquery"
     },
     shim: {
 
