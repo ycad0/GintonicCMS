ee4a10c3472cccf2411a7a4ae0712e77df51d5cb
Philippe Lafrance <phil.laf@gmail.com>
2015-07-07 14:05:46 -0400

    travis tests updated

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,41 +1,55 @@
 <?php
 // @codingStandardsIgnoreFile
-use Cake\Cache\Cache;
-use Cake\Core\Configure;
-use Cake\Network\Email\Email;
-use Cake\Core\Plugin;
-use Cake\Datasource\ConnectionManager;
-use Cake\I18n\I18n;
-require_once 'vendor/autoload.php';
+
+$findRoot = function () {
+    $root = dirname(__DIR__);
+    if (is_dir($root . '/vendor/cakephp/cakephp')) {
+        return $root;
+    }
+    $root = dirname(dirname(__DIR__));
+    if (is_dir($root . '/vendor/cakephp/cakephp')) {
+        return $root;
+    }
+    $root = dirname(dirname(dirname(__DIR__)));
+    if (is_dir($root . '/vendor/cakephp/cakephp')) {
+        return $root;
+    }
+};
 
 // Path constants to a few helpful things.
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
 
-define('ROOT', dirname(__DIR__) . DS);
-define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
-define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
-define('CAKE', CORE_PATH . 'src' . DS);
-define('TESTS', ROOT . 'tests');
-define('APP', TESTS . DS . 'App');
-define('APP_DIR', 'app');
+define('ROOT', $findRoot());
+define('APP_DIR', 'App');
 define('WEBROOT_DIR', 'webroot');
-define('WWW_ROOT', dirname(APP) . DS . 'webroot' . DS);
-define('TMP', sys_get_temp_dir() . DS);
-define('CONFIG', ROOT . 'tests' . DS . 'config' . DS);
-define('CACHE', TMP);
-define('LOGS', TMP);
+define('APP', ROOT . '/tests/App/');
+define('TESTS', ROOT . DS . 'tests' . DS);
+define('CONFIG', TESTS . 'config' . DS);
+define('WWW_ROOT', ROOT . DS . WEBROOT_DIR . DS);
+define('TMP', ROOT . DS . 'tmp' . DS);
+define('LOGS', TMP . 'logs' . DS);
+define('CACHE', TMP . 'cache' . DS);
+define('CAKE_CORE_INCLUDE_PATH', ROOT . '/vendor/cakephp/cakephp');
+define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+define('CAKE', CORE_PATH . 'src' . DS);
 
-require_once CORE_PATH . 'config/bootstrap.php';
-
-date_default_timezone_set('UTC');
-mb_internal_encoding('UTF-8');
-Configure::write('debug', true);
+require ROOT 'vendor/autoload.php';
+require CORE_PATH . 'config/bootstrap.php';
 
 Cake\Core\Configure::write('App', ['namespace' => 'GintonicCMS\Test\App']);
+Configure::write('debug', true);
+
+$TMP = new \Cake\Filesystem\Folder(TMP);
+$TMP->create(TMP . 'cache/models', 0777);
+$TMP->create(TMP . 'cache/persistent', 0777);
+$TMP->create(TMP . 'cache/views', 0777);
 
 Cache::config([
+    'default' => [
+        'engine' => 'File'
+    ],
     '_cake_core_' => [
         'engine' => 'File',
         'prefix' => 'cake_core_',
@@ -48,24 +62,30 @@ Cache::config([
     ]
 ]);
 
-if (!getenv('db_dsn')) {
-    //putenv('db_dsn=postgres://postgres@127.0.0.1/cakephp_test');
-    putenv('db_dsn=sqlite:///:memory:');
-}
-Cake\Datasource\ConnectionManager::config('test', [
-    'url' => getenv('db_dsn'),
-    'timezone' => 'UTC'
+Configure::write([
+    'Acl.classname' => 'DbAcl',
+    'Acl.database' => 'default',
+    'Gintonic.website.name' => 'GintonicCMS'
 ]);
 
-Plugin::load('GintonicCMS', ['path' => ROOT]);
-
-Configure::write('Acl.classname', 'DbAcl');
-Configure::write('Acl.database', 'default');
 Configure::load('email');
 //Configure::load('app');
 Email::configTransport(Configure::consume('EmailTransport'));
 Email::config(Configure::consume('Email'));
 
-Configure::write('Gintonic.website.name', 'GintonicCMS');
+if (!getenv('db_dsn')) {
+    //putenv('db_dsn=postgres://postgres@127.0.0.1/cakephp_test');
+    putenv('db_dsn=sqlite:///:memory:');
+}
+
+Plugin::load('GintonicCMS', ['path' => ROOT]);
+
 Cake\Routing\DispatcherFactory::add('Routing');
 Cake\Routing\DispatcherFactory::add('ControllerFactory');
+
+date_default_timezone_set('UTC');
+mb_internal_encoding('UTF-8');
+Cake\Datasource\ConnectionManager::config('test', [
+    'url' => getenv('db_dsn'),
+    'timezone' => 'UTC'
+]);
