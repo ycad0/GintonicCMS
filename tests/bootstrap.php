78a9e77e80b69769bb3f69ffdc4e99d49115a9c0
Philippe Lafrance <phil.laf@gmail.com>
2015-08-08 19:45:34 -0400

    Almost everything is being moved out of the plugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,6 @@
 // @codingStandardsIgnoreFile
 use Cake\Cache\Cache;
 use Cake\Core\Configure;
-use Cake\Network\Email\Email;
 use Cake\Core\Plugin;
 use Cake\Datasource\ConnectionManager;
 use Cake\I18n\I18n;
@@ -16,50 +15,33 @@ define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakeph
 define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
 define('CAKE', CORE_PATH . 'src' . DS);
 define('TESTS', ROOT . 'tests');
-define('APP', ROOT . 'tests' . DS);
+define('APP', ROOT . 'tests' . DS . 'test_files' . DS . 'app' . DS);
 define('APP_DIR', 'app');
 define('WEBROOT_DIR', 'webroot');
 define('WWW_ROOT', dirname(APP) . DS . 'webroot' . DS);
 define('TMP', sys_get_temp_dir() . DS);
-define('CONFIG', ROOT . 'tests' . DS . 'config' . DS);
+define('CONFIG', dirname(APP) . DS . 'config' . DS);
 define('CACHE', TMP);
 define('LOGS', TMP);
-
 require_once CORE_PATH . 'config/bootstrap.php';
-require APP . 'App/Controller/AppController.php';
-require APP . 'App/Controller/Admin/AppController.php';
-
 date_default_timezone_set('UTC');
 mb_internal_encoding('UTF-8');
 Configure::write('debug', true);
-Cake\Core\Configure::write('App', ['namespace' => 'GintonicCMS\Test\App']);
-Cache::config([
-    '_cake_core_' => [
-        'engine' => 'File',
-        'prefix' => 'cake_core_',
-        'serialize' => true
-    ],
-    '_cake_model_' => [
-        'engine' => 'File',
-        'prefix' => 'cake_model_',
-        'serialize' => true
+Configure::write('App', [
+    'namespace' => 'App',
+    'encoding' => 'UTF-8',
+    'base' => false,
+    'baseUrl' => false,
+    'dir' => 'src',
+    'webroot' => WEBROOT_DIR,
+    'www_root' => WWW_ROOT,
+    'fullBaseUrl' => 'http://localhost',
+    'imageBaseUrl' => 'img/',
+    'jsBaseUrl' => 'js/',
+    'cssBaseUrl' => 'css/',
+    'paths' => [
+        'plugins' => [dirname(APP) . DS . 'plugins' . DS],
+        'templates' => [APP . 'Template' . DS]
     ]
 ]);
-
-if (!getenv('db_dsn')) {
-    //putenv('db_dsn=postgres://postgres@127.0.0.1/cakephp_test');
-    putenv('db_dsn=sqlite:///:memory:');
-}
-Cake\Datasource\ConnectionManager::config('test', [
-    'url' => getenv('db_dsn'),
-    'timezone' => 'UTC'
-]);
-
 Plugin::load('GintonicCMS', ['path' => ROOT]);
-
-Configure::write('Acl.classname', 'DbAcl');
-Configure::write('Acl.database', 'default');
-Configure::write('Gintonic.website.name', 'GintonicCMS');
-
-Cake\Routing\DispatcherFactory::add('Routing');
-Cake\Routing\DispatcherFactory::add('ControllerFactory');
