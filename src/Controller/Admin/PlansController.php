1efc25e624f4b7e9c992bd39fa322d1ac37d9a57
Yan <y.cadoret@gmail.com>
2015-08-12 23:29:06 -0400

    Changements din routes 2.

---
+++ src/Controller/Admin/PlansController.php
@@ -0,0 +1,13 @@
+<?php
+namespace GintonicCMS\Controller\Admin;
+
+use App\Controller\Admin\AppController;
+
+/**
+ * Plans Controller
+ *
+ * @property \GintonicCMS\Model\Table\UsersTable $Plans
+ */
+class PlansController extends AppController
+{
+}
